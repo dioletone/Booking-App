37070fc350e390b75dd035e1f44c4ec8cd718500
dioletone <87907834+dioletone@users.noreply.github.com>
2024-07-12 14:42:22 +0700

    Delete client/components directory

--- client/components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import '../../styles/Logo.css'
-export function Logo() {
-    return (
-        <div className="logo">
-            <img src="/airbnb.png" alt="Airbnb Logo" />        </div>
-    )
-}
-export default Logo;
